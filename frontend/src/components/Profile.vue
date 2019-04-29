73bf86653fb7d8b47204aa2d4770036fdb4f9f02
VioletKaori <835243490@qq.com>
2019-04-29 18:04:23 +0800

    个人信息页

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      msg: 'Profile'
+    }
+  },
+  methods: {
+  }
+}
+</script>
