197e14dbb462a2c057e6ce32a2876a6bb5aabe17
VioletKaori <835243490@qq.com>
2019-04-29 18:06:41 +0800

    配置路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,25 +3,62 @@ import Router from 'vue-router'
 import Login from '@/views/Login'
 import Register from '@/views/Register'
 import Main from '@/views/Main'
+import Home from '@/components/Home'
+import Wifi from '@/components/Wifi'
+import Settings from '@/components/Settings'
+import Account from '@/components/Account'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      redirect: '/login',
+      meta: {
+        title: '物联网数据服务平台'
+      }
+    },
+    {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
+      meta: {
+        title: '登录'
+      }
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
+      meta: {
+        title: '注册'
+      }
     },
     {
       path: '/main',
       name: 'Main',
-      component: Main
+      component: Main,
+      meta: {
+        title: '主页'
+      },
+      children: [
+        { path: 'Home', component: Home },
+        { path: 'Wifi', component: Wifi },
+        {
+          path: 'Settings',
+          component: Settings,
+          children: [
+            {
+              path: 'Account', component: Account
+            },
+            {
+              path: 'Profile', component: Profile
+            }
+          ]
+        }
+      ]
     }
   ]
 })
