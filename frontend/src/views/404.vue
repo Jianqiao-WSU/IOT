f65b5871bc7f93eb75b5ae8e3ac0d52102a66c44
VioletKaori <835243490@qq.com>
2019-05-02 19:41:38 +0800

    添加404页面

---
+++ frontend/src/views/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pic-404">
+    <img class="pic-404__parent" src="@/assets/404.png" alt="404">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page404',
+  data () {
+    return {
+      msg: 'The webmaster said that you can not enter this page...'
+    }
+  }
+}
+</script>
